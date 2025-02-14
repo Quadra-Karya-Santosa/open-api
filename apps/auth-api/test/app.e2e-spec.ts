5f9131c9d54ecf474b9eca643a3df01caba5ea6c
dionisius7 <dionisiusadityaoctanugraha@gmail.com>
2025-02-14 20:07:10 +0700

    fix: renaming file

--- apps/auth-api/test/app.e2e-spec.ts
+++ apps/auth-api/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AuthApiModule } from './../src/auth-api.module';
+import { AppModule } from '../src/app.module';
 
 describe('AuthApiController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AuthApiModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
